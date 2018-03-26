aa176ee785413f12507592b5dcd7029e2cee4fa7
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-03-26 21:56:26 +0200

    Add new lines

--- slices/fold.go
+++ slices/fold.go
@@ -8,11 +8,13 @@ import (
 // Fold slice to single value using the predicate function.
 func Fold(input, predicate interface{}) (interface{}, error) {
 	var output reflect.Value
+
 	l, item := source(input)
 
 	if l == 0 {
 		return nil, errors.New("Not a valid input slice")
 	}
+
 	iv := reflect.ValueOf(predicate)
 
 	for i := 0; i < l; i++ {
