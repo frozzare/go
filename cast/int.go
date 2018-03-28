c2f3ea6fbceaa7a0f727c13ef36ae8cbd83ea6e1
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2018-03-28 08:40:37 +0200

    Add Int64 to cast

--- cast/int.go
+++ cast/int.go
@@ -6,7 +6,70 @@ import (
 )
 
 // Int will converts argument to int64 or return a error.
-func Int(value interface{}) (int64, error) {
+func Int(value interface{}) (int, error) {
+	switch v := value.(type) {
+	case bool:
+		if v {
+			return int(1), nil
+		}
+
+		return int(0), nil
+	case float32:
+		return int(v), nil
+	case float64:
+		return int(v), nil
+	case int:
+		return int(v), nil
+	case int8:
+		return int(v), nil
+	case int16:
+		return int(v), nil
+	case int32:
+		return int(v), nil
+	case int64:
+		return int(v), nil
+	case uint:
+		return int(v), nil
+	case uint8:
+		return int(v), nil
+	case uint16:
+		return int(v), nil
+	case uint32:
+		return int(v), nil
+	case uint64:
+		return int(v), nil
+	case string:
+		f, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return int(0), nil
+		}
+		return int(f), nil
+	case []byte:
+		f, err := strconv.ParseFloat(string(v), 64)
+		if err != nil {
+			return int(0), nil
+		}
+		return int(f), nil
+	case nil:
+		return int(0), nil
+	default:
+		return int(0), errors.New("Unknown type")
+	}
+}
+
+// MustInt converts argument to int or panic if an error occurred.
+func MustInt(value interface{}) int {
+	v, err := Int(value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+// Int64 will converts argument to int64 or return a error.
+func Int64(value interface{}) (int64, error) {
 	switch v := value.(type) {
 	case bool:
 		if v {
@@ -57,9 +120,9 @@ func Int(value interface{}) (int64, error) {
 	}
 }
 
-// MustInt converts argument to int or panic if an error occurred.
-func MustInt(value interface{}) int64 {
-	v, err := Int(value)
+// MustInt64 converts argument to int or panic if an error occurred.
+func MustInt64(value interface{}) int64 {
+	v, err := Int64(value)
 
 	if err != nil {
 		panic(err)
